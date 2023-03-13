3a4306add389ab5ba71c11a9535caf0c3b806c13
Wendolfgan <96495549+Wendolfgan@users.noreply.github.com>
2023-03-13 16:32:12 +0300

    Add models of authors and keywords

--- Django-Web-E_Library-main/unversityLib/main/models.py
+++ Django-Web-E_Library-main/unversityLib/main/models.py
@@ -4,3 +4,25 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 import datetime
 
+
+class Author(models.Model):
+    name = models.CharField("Автор", max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Автор'
+        verbose_name_plural = 'Авторы'
+
+
+class Keyword(models.Model):
+    keyword_name = models.CharField("Ключевое слово", max_length=250)
+
+    def __str__(self):
+        return self.keyword_name
+
+    class Meta:
+        verbose_name = 'Ключевое слово'
+        verbose_name_plural = 'Ключевые слова'
+
