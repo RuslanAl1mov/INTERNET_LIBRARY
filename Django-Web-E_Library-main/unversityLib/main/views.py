2a5ae69e74e06bfb424f390337cd237998dd5833
Borisborisss <96425850+Borisborisss@users.noreply.github.com>
2023-03-13 16:37:09 +0300

    add index generation code

--- Django-Web-E_Library-main/unversityLib/main/views.py
+++ Django-Web-E_Library-main/unversityLib/main/views.py
@@ -45,3 +45,44 @@ def authors_for_menu_dropdown():
 
     return authors_block
 
+
+
+def index(request):
+    books_list = Books.objects.all()[::-1]
+    IMAGES_PATH = f'unversityLib\\static\\img\\books_covers_imgs\\'
+
+    books_info_list = []
+
+    if len(books_list) != 0:
+        book_number = 0
+        for book in books_list:
+            book_number += 1
+            if book_number <= 9:
+                book_name = book.file.name.split('/')[-1]
+                book_format = book_name.split('.')[-1]
+                file_size = os.path.getsize(book.file.name) / (1024 * 1024)
+                file_size = float(f'{file_size:.2f}')
+                try:
+                    image_name = book_name.replace('.pdf', '') + '.png'
+                    if image_name not in os.listdir(IMAGES_PATH):
+                        doc = fitz.open(book.file.name)
+                        pix = doc.get_page_pixmap(0)
+                        pix.save(IMAGES_PATH + image_name)
+
+                        image = Image.open(IMAGES_PATH + image_name)
+                        resized_image = image.resize((245, 330))
+                        resized_image.save(IMAGES_PATH + image_name)
+
+                except Exception as e:
+                    print(e)
+                    image_name = 'UnknownBookCover.png'
+
+                books_info_list.append({'book_object': book, 'book_cover_name': image_name, 'file_format': book_format,
+                                        'file_size': file_size})
+
+    return render(request, 'main/index.html', {'books_list': books_info_list,
+                                               'keywords_block': keywords_for_menu_dropdown(),
+                                               'authors_block': authors_for_menu_dropdown(),
+                                               'books_amount': len(books_list)})
+
+
